c77cb5ae9cbeee9685f0048ef90e0e84b4c58f41
MahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2026-09-10 20:59:04 -0400

    finish

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -11,6 +11,17 @@ def emotion_detector(text_to_analyze):
     
     response = requests.post(url,headers=headers,json=obj)
     
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
+
     response_json = json.loads(response.text)
 
     emotions = ["anger","disgust","fear","joy","sadness"]
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
+"""Flask Server"""
 from flask import Flask,request,render_template
 from EmotionDetection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route("/emotionDetector", methods=["GET"])
-def emotionDetector():
+def emotion_detection_route():
+    """/emotionDetector api path"""
     text = request.args.get("textToAnalyze")
 
     detection = emotion_detector(text)
@@ -15,15 +17,21 @@ def emotionDetector():
     sadness = detection['sadness']
     dominant_emotion = detection['dominant_emotion']
 
-    formated_text = f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!."
+
+    formated_text = f"For the given statement, the system response is 'anger': {anger}"
+    formated_text += f", 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and "
+    formated_text += f"'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
     return formated_text
 
 
 @app.route("/")
 def home():
+    """root page"""
     return render_template("index.html")
 
 
-if(__name__ == '__main__'):
+if __name__ == '__main__':
     app.run(debug = True,host="0.0.0.0",port=5000)
