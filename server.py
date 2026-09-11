992ccab75c8158d7f8bed46d4d294474e6c59049
MahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2026-09-10 20:40:17 -0400

    flask deployment

--- server.py
+++ server.py
@@ -8,7 +8,15 @@ def emotionDetector():
     text = request.args.get("textToAnalyze")
 
     detection = emotion_detector(text)
-    formated_text = f"For the given statement, the system response is 'anger': {detection["anger"]}, 'disgust': {detection["disgust"]}, 'fear': {detection["fear"]}, 'joy': {detection["joy"]} and 'sadness': {detection["sadness"]}. The dominant emotion is {detection["dominant_emotion"]}. "
+    anger = detection['anger']
+    disgust = detection['disgust']
+    fear = detection['fear']
+    joy = detection['joy']
+    sadness = detection['sadness']
+    dominant_emotion = detection['dominant_emotion']
+
+    formated_text = f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+
     return formated_text
 
 
@@ -18,4 +26,4 @@ def home():
 
 
 if(__name__ == '__main__'):
-    app.run(debug = True)
+    app.run(debug = True,host="0.0.0.0",port=5000)
